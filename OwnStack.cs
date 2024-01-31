64368e4e9f29b88e43ffa9aa6b3888d0c09b5bc0
Oleksii <oleksiy26032005@gmail.com>
2024-01-31 13:58:53 +0200

    Добавьте файлы проекта.

---
+++ OwnStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnStack
+{
+    public class OwnStack
+    {
+        private object[] _items;
+        
+        public int Count {  get; private set; } // К-сть дійсно зберігаємих елементів в _items (по замовчувані = 0)
+        public int Capacity // К-сть максимально можливих  елементів в _items (по замовчувані = defaultCapacity)
+        {
+            get
+            {
+                return _items.Length;
+            }
+        }
+
+        public OwnStack() 
+        { 
+            const int defaultCapacity = 4;
+            _items = new object[defaultCapacity];
+        }
+
+        public OwnStack(int capacity)
+        {
+            _items = new object[capacity];
+        }
+
+        public void Push(object item)
+        {
+            if(Capacity == Count)
+            {
+                object[] doubleItems = new object[Count * 2];
+                Array.Copy(_items, doubleItems, Count);
+
+                _items = doubleItems;
+            }
+
+            _items[Count++] = item;
+        }
+
+        public void Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _items[--Count] = null;
+        }
+
+        public object Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _items[Count - 1];
+        }
+
+        public void Print ()
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                Console.WriteLine($"items[{i}] = {_items[i]}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OwnStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OwnStack instance = new OwnStack();
+
+            instance.Push(1);
+            instance.Push(2);
+            instance.Push(3);
+
+            Console.WriteLine($"Останiй добавлений елемент = {instance.Peek()}\n");
+
+            instance.Push(4);
+
+            instance.Print();
+
+            Console.WriteLine($"\nCount = {instance.Count}\nCapacity = {instance.Capacity}\n");
+
+            instance.Push(5);
+
+            instance.Print();
+
+            Console.WriteLine($"\nCount = {instance.Count}\nCapacity = {instance.Capacity}\n");
+
+            Console.WriteLine("\nРобимо instance.Pop()");
+            
+            instance.Pop();
+
+            instance.Print();
+
+            Console.WriteLine($"\nCount = {instance.Count}\nCapacity = {instance.Capacity}");
+
+        }
+    }
+}
